7cbd1a9ae078a1b9315961111129f8508d3674d3
huameiwei-vc <125441847+huameiwei-vc@users.noreply.github.com>
2023-07-05 18:00:37 +0800

    Update Logo.js
    3

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -2,8 +2,7 @@
 import * as React from 'react'
 
 const Logo = (props) => (
-<svg version="1.1" id="图层_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+<svg version="1.1" id="图层_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
 <style type="text/css">
 	.st0{fill:#FD735C;}
 	.st1{fill:#211C16;}
@@ -193,7 +192,6 @@ const Logo = (props) => (
 	<path class="st0" d="M7.4,66.6c-0.5,0.3,0,1.1,0.6,0.7"/>
 </g>
 </svg>
-
 )
 
 export default Logo
