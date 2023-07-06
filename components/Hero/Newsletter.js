357a1a1045e5bf8bc2fa056f9b6e7699d802bf63
huameiwei-vc <125441847+huameiwei-vc@users.noreply.github.com>
2023-07-06 17:26:23 +0800

    Update Newsletter.js
    1

--- components/Hero/Newsletter.js
+++ components/Hero/Newsletter.js
@@ -23,17 +23,6 @@ const NewsletterHero = ({ blockMap }) => {
   return (
     <>
       <div className='container mx-auto flex px-5 py-2 mb-10 md:flex-row flex-col items-center'>
-        <div className='flex flex-col md:w-4/5 md:items-start mb-6 md:mb-0 md:text-left'>
-          <NotionRenderer
-            className='md:ml-0'
-            blockMap={blockMap}
-            frontMatter={{}}
-            subPageTitle={null}
-          />
-          <Social />
-         
-        </div>
-      
         
       </div>
     </>
